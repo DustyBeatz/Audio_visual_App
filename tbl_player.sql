7e1ac721ea8e7dcee590a9a6e7c0ad14c255edea
Reece Devine <reece.devine97@gmail.com>
2018-11-30 14:35:36 -0400

    added data base

---
+++ tbl_player.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 30, 2018 at 06:34 PM
+-- Server version: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_video_App`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_player`
+--
+
+DROP TABLE IF EXISTS `tbl_player`;
+CREATE TABLE IF NOT EXISTS `tbl_player` (
+  `id` int(150) NOT NULL AUTO_INCREMENT,
+  `video_name` varchar(150) NOT NULL,
+  `video_duration` varchar(10) NOT NULL,
+  `video_desc` varchar(350) NOT NULL,
+  `video_thumbnail` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
